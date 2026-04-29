965b8a9298f65b689a05d47c253987042313a97d
Blanadi <vadym_moroz1804@tntu.edu.ua>
2026-04-29 18:24:01 +0300

    Repair

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,11 +8,11 @@ public class CalculatorTest {
 
     @Test
     void testAdd() {
-        assertEquals(55, calc.add(2, 3));
+        assertEquals(5, calc.add(2, 3));
     }
 
     @Test
     void testMultiply() {
-        assertEquals(63, calc.multiply(2, 3));
+        assertEquals(6, calc.multiply(2, 3));
     }
 }
